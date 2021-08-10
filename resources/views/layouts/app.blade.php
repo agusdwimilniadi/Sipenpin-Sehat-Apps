7632b03d51b7e6d7f53a32f91509a27a9907d1e2
Agus Dwi <agusdwimill@gmail.com>
2021-08-10 14:36:30 +0700

    Tambah livechat, testing

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -82,8 +82,10 @@
     </script>
     @laravelPWA
     
-
+    <script type="text/javascript">window.$crisp=[];window.CRISP_WEBSITE_ID="ec91f130-1c98-4c62-a738-924b6d085117";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();</script>
 </head>
+
+
 <body>
     
     <div id="app">
